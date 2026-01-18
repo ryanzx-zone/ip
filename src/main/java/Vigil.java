bec3d7d3dad85b2b9421cadb3457c760a32462f9
ryanzx-zone <rpangzx@gmail.com>
2026-01-18 18:07:07 +0800

    Level-0: Rename chatbot to Vigil, greet and exit

---
+++ src/main/java/Vigil.java
@@ -0,0 +1,21 @@
+public class Vigil {
+    public static void main(String[] args) {
+        String logo = """
+                ██╗   ██╗██╗ ██████╗ ██╗██╗
+                ██║   ██║██║██╔════╝ ██║██║
+                ██║   ██║██║██║  ███╗██║██║
+                ╚██╗ ██╔╝██║██║   ██║██║██║
+                 ╚████╔╝ ██║╚██████╔╝██║███████╗
+                  ╚═══╝  ╚═╝ ╚═════╝ ╚═╝╚══════╝
+                """;
+
+
+        System.out.println(logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Vigil");
+        System.out.println("What can I do for you today?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Goodbye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
