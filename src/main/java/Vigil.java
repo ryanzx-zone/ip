38dd9b473db5967f87ed7e037195db439081da50
ryanzx-zone <rpangzx@gmail.com>
2026-01-30 18:33:04 +0800

    Level-1: Echo

--- src/main/java/Vigil.java
+++ src/main/java/Vigil.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Vigil {
     public static void main(String[] args) {
         String logo = """
@@ -15,7 +17,26 @@ public class Vigil {
         System.out.println("Hello! I'm Vigil");
         System.out.println("What can I do for you today?");
         System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String line = scanner.nextLine();
+
+            if (line.equalsIgnoreCase("bye")) {
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + line);
+            System.out.println("____________________________________________________________");
+        }
+
+        System.out.println("____________________________________________________________");
         System.out.println("Goodbye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+
+        scanner.close();
     }
 }
+
