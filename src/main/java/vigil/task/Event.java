dd8cc33f2c691774ab75b101627c4dad7ac7399e
ryanzx-zone <rpangzx@gmail.com>
2026-02-23 13:34:40 +0800

    Event: Add LocalDate support for deadline dates. Parse deadline dates as LocalDate when in yyyy-MM-dd format and display in MMM d yyyy format.

--- src/main/java/vigil/task/Event.java
+++ src/main/java/vigil/task/Event.java
@@ -1,13 +1,24 @@
 package vigil.task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Event extends Task {
+
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM d yyyy");
+
     protected String from;
     protected String to;
+    protected LocalDate fromDate;
+    protected LocalDate toDate;
 
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
+        this.fromDate = parseDate(from);
+        this.toDate = parseDate(to);
     }
 
     public String getFrom() {
@@ -18,8 +29,18 @@ public class Event extends Task {
         return to;
     }
 
+    private static LocalDate parseDate(String raw) {
+        try {
+            return LocalDate.parse(raw);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
+    }
+
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        String displayFrom = (fromDate != null) ? fromDate.format(OUTPUT_FORMAT) : from;
+        String displayTo = (toDate != null) ? toDate.format(OUTPUT_FORMAT) : to;
+        return "[E]" + super.toString() + " (from: " + displayFrom + " to: " + displayTo + ")";
     }
 }
